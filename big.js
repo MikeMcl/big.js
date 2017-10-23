c444cd358e7dd70d4912cac2a3470b9419a393ff
Michael Mclaughlin <M8ch88l@gmail.com>
2017-10-23 22:50:17 +0100

    #89 Optimisation of internal round function

--- big.js
+++ big.js
@@ -59,6 +59,7 @@
     NAME = '[big.js] ',
     INVALID = NAME + 'Invalid ',
     INVALID_DP = INVALID + 'decimal places',
+    INVALID_RM = INVALID + 'rounding mode',
     DIV_BY_ZERO = NAME + 'Division by zero',
 
     // The shared prototype object.
@@ -181,51 +182,57 @@
     var xc = x.c,
       i = x.e + dp + 1;
 
-    if (rm === 1) {
+    if (i < xc.length) {
+      if (rm === 1) {
 
-      // xc[i] is the digit after the digit that may be rounded up.
-      more = xc[i] >= 5;
-    } else if (rm === 2) {
-      more = xc[i] > 5 || xc[i] == 5 && (more || i < 0 || xc[i + 1] !== UNDEFINED || xc[i - 1] & 1);
-    } else if (rm === 3) {
-      more = more || xc[i] !== UNDEFINED || i < 0;
-    } else {
-      more = false;
-      if (rm !== 0) throw Error(INVALID + 'rounding mode');
-    }
+        // xc[i] is the digit after the digit that may be rounded up.
+        more = xc[i] >= 5;
+      } else if (rm === 2) {
+        more = xc[i] > 5 || xc[i] == 5 &&
+          (more || i < 0 || xc[i + 1] !== UNDEFINED || xc[i - 1] & 1);
+      } else if (rm === 3) {
+        more = more || xc[i] !== UNDEFINED || i < 0;
+      } else {
+        more = false;
+        if (rm !== 0) throw Error(INVALID_RM);
+      }
 
-    if (i < 1 || !xc[0]) {
-      xc.length = 1;
-      if (more) {
+      if (i < 1) {
+        xc.length = 1;
 
-        // 1, 0.1, 0.01, 0.001, 0.0001 etc.
-        x.e = -dp;
-        xc[0] = 1;
+        if (more) {
+
+          // 1, 0.1, 0.01, 0.001, 0.0001 etc.
+          x.e = -dp;
+          xc[0] = 1;
+        } else {
+
+          // Zero.
+          xc[0] = x.e = 0;
+        }
       } else {
 
-        // Zero.
-        xc[0] = x.e = 0;
-      }
-    } else {
+        // Remove any digits after the required decimal places.
+        xc.length = i--;
 
-      // Remove any digits after the required decimal places.
-      xc.length = i--;
+        // Round up?
+        if (more) {
 
-      // Round up?
-      if (more) {
-
-        // Rounding up may mean the previous digit has to be rounded up.
-        for (; ++xc[i] > 9;) {
-          xc[i] = 0;
-          if (!i--) {
-            ++x.e;
-            xc.unshift(1);
+          // Rounding up may mean the previous digit has to be rounded up.
+          for (; ++xc[i] > 9;) {
+            xc[i] = 0;
+            if (!i--) {
+              ++x.e;
+              xc.unshift(1);
+            }
           }
         }
-      }
 
-      // Remove trailing zeros.
-      for (i = xc.length; !xc[--i];) xc.pop();
+        // Remove trailing zeros.
+        for (i = xc.length; !xc[--i];) xc.pop();
+      }
+    } else if (rm < 0 || rm > 3 || rm !== ~~rm) {
+      throw Error(INVALID_RM);
     }
 
     return x;
